cf77354e94b0a57a0f2aed65204e9a4a6b16806a
Vlado Ivkovic <ivkovicv90@gmail.com>
2024-01-17 19:26:30 +0100

    fixed restart

--- main.js
+++ main.js
@@ -6,12 +6,56 @@ let displayResult = $(".display__result");
 
 let allText = [];
 let score = 0;
-let speed = 0;
+let speed = 1;
 let textLength = 3;
 let gameEnd = false;
+let lvl = 6;
+let typingWords;
 $(reset).hide();
 
 startBtn.on("click", startGame);
+
+function checkInputTyping() {
+	let inputVal = $(this).val();
+	let self = $(this);
+	if (allText.includes(inputVal)) {
+		let index = allText.indexOf(inputVal);
+		allText.splice(index, 1);
+		$("span")
+			.filter(function () {
+				return $(this).text() == inputVal;
+			})
+			.css("background", "lightblue")
+			.fadeOut(100, function () {
+				$(this).remove();
+			});
+		self.val("");
+		score++;
+		displayResult.html(score);
+	}
+}
+
+//insert spans
+function insertSpans() {
+	if (gameEnd) return;
+	for (var i = 0; i < allLines.length; i++) {
+		let rand = Math.floor(Math.random() * 20);
+		if (rand <= lvl) {
+			let text = chooseText();
+			allText.push(text);
+			$(allLines[i]).append(`<span>${text}</span>`);
+		}
+	}
+	setTimeout(insertSpans, 7000);
+}
+
+function chooseText() {
+	let rand = Math.floor(Math.random() * typingWords.length);
+	let savedText = typingWords[rand];
+	typingWords.splice(rand, 1);
+
+	return savedText;
+}
 
 function startGame() {
 	$(this).hide();
@@ -19,9 +63,7 @@ function startGame() {
 	mainInput.focus();
 
 	//setup
-	speed = 1;
-	let typingWords = words.filter((word) => word.length == textLength);
-	let lvl = 6;
+	typingWords = words.filter((word) => word.length == textLength);
 
 	let speedUp = setInterval(function () {
 		textLength++;
@@ -29,48 +71,7 @@ function startGame() {
 	}, 20000);
 
 	mainInput.on("keyup", checkInputTyping);
-	function checkInputTyping() {
-		let inputVal = $(this).val();
-		let self = $(this);
-		if (allText.includes(inputVal)) {
-			let index = allText.indexOf(inputVal);
-			allText.splice(index, 1);
-			$("span")
-				.filter(function () {
-					return $(this).text() == inputVal;
-				})
-				.css("background", "lightblue")
-				.fadeOut(100, function () {
-					$(this).remove();
-				});
-			self.val("");
-			score++;
-			displayResult.html(score);
-		}
-	}
-
-	//insert spans
-	function insertSpans() {
-		if (gameEnd) return;
-		for (var i = 0; i < allLines.length; i++) {
-			let rand = Math.floor(Math.random() * 20);
-			if (rand <= lvl) {
-				let text = chooseText();
-				allText.push(text);
-				$(allLines[i]).append(`<span>${text}</span>`);
-			}
-		}
-		setTimeout(insertSpans, 7000);
-	}
 	insertSpans();
-
-	function chooseText() {
-		let rand = Math.floor(Math.random() * typingWords.length);
-		let savedText = typingWords[rand];
-		typingWords.splice(rand, 1);
-
-		return savedText;
-	}
 
 	//animacija spanova
 	let moveAll = setInterval(function () {
@@ -85,7 +86,6 @@ function startGame() {
 			if (position > 850) {
 				clearAllIntervals();
 				gameEnd = true;
-				clearInterval(speedUp);
 			} else if (position > 700 && position < 710) {
 				$(el).addClass("danger");
 			}
@@ -94,16 +94,22 @@ function startGame() {
 
 	function clearAllIntervals() {
 		clearInterval(moveAll);
+		clearInterval(speedUp);
+	}
+
+	if (gameEnd) {
+		clearAllIntervals();
+		gameEnd = false;
 	}
 }
 
 reset.on("click", function () {
-	gameEnd = false;
+	gameEnd = true;
 	displayResult.html(0);
 	$("span").remove();
 	allText = [];
 	score = 0;
-	speed = 0;
+	speed = 1;
 	textLength = 3;
 	startGame();
 });
